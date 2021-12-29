a708afcb08df5fcc9efeabad854a6f19c47c8da9
shriwatri <40926302+shriwatri@users.noreply.github.com>
2021-12-29 13:13:15 +0530

    Sum String
    int sum =	Integer.parseInt(s1)+Integer.parseInt(s2);
    String.valueOf(sum)

---
+++ SumString.java
@@ -0,0 +1,12 @@
+
+public class SumString {
+
+	public static void main(String[] args) {
+	String s1="111", s2="222";
+	int sum =	Integer.parseInt(s1)+Integer.parseInt(s2);
+	System.out.println("Sum:"+sum);
+	System.out.println("String.valueOf(sum) Sum:"+String.valueOf(sum));
+
+	}
+
+}
