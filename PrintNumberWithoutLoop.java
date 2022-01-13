6346eeb08761c2ec6954d94c24a11f0b9f5cfe09
shriwatri <40926302+shriwatri@users.noreply.github.com>
2022-01-13 12:14:53 +0530

    Print Number without loop and using recursion

---
+++ PrintNumberWithoutLoop.java
@@ -0,0 +1,16 @@
+
+public class PrintNumberWithoutLoop {
+
+	public static void main(String[] args) {
+		printNumber(1);
+
+	}
+	
+	public static void printNumber(int n) {
+		if(n <= 10) {
+			System.out.println(n);
+			printNumber(n+1);
+		}
+	}
+
+}
