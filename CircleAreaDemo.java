6320a45647d6e099dc431eef1de52d77025b4bd8
shriwatri <40926302+shriwatri@users.noreply.github.com>
2022-01-22 17:20:48 +0530

    Circle Area

---
+++ CircleAreaDemo.java
@@ -0,0 +1,15 @@
+
+public class CircleAreaDemo {
+
+	public static void main(String[] args) {
+		double radious = 7;
+		
+		double area = Math.PI*(radious*radious);
+		System.out.println("Area Math.PI*(radious*radious)="+area);
+		
+		double circum = 2*Math.PI*radious;
+		System.out.println("Cicumtance 2*Math.PI*radious="+circum);
+
+	}
+
+}
