b33eaac4df06ab30b0c31ec84c427621fc3b869a
shriwatri <40926302+shriwatri@users.noreply.github.com>
2022-02-01 15:01:06 +0530

    left rotate array

---
+++ LeftRotateArray.java
@@ -0,0 +1,19 @@
+
+public class LeftRotateArray {
+
+	public static void main(String[] args) {
+		int[] array = {10,20,30,40,50};
+		int temp = array[0];
+		
+		for (int i = 1; i < array.length; i++) {
+			array[i-1]=array[i];
+			
+		}
+		array[array.length-1]=temp;
+		for (int i : array) {
+			System.out.println(i);
+		}
+
+	}
+
+}
