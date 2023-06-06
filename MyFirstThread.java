28bd76eff6cc50ccf3f550c11b9ffe6c3fa4690d
shriwatri <40926302+shriwatri@users.noreply.github.com>
2023-06-06 22:25:59 +0530

    Thread Demo
    implement runnable interface

---
+++ MyFirstThread.java
@@ -0,0 +1,38 @@
+package com.shriwatri;
+
+public class MyFirstThread {
+
+	public static void main(String[] args) {
+		Task task = new Task();
+		Thread thread = new Thread(task);
+		thread.start();
+		System.out.println("Insdie main...");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Insdie main after sleep method...");
+	}
+}
+
+class Task implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("Insdie run method..");
+		go();
+		
+	}
+
+	private void go() {
+		System.out.println("Insdie go method..");
+		more();
+	}
+	
+	private void more() {
+		System.out.println("Insdie more method..");
+		
+	}
+	
+}
